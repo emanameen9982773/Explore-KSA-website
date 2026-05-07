3a61e6d6214f0aebdc0cf7cf8aa720ad1ab8011a
emanameen9982773 <emanothard15@gmail.com>
2026-05-07 06:48:03 +0300

    rename files

---
+++ public_pages/regionsGallary.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+    <title>Region Gallary</title>
+</head>
+
+<body>
+
+    <header>
+        <div>
+            <nav>
+                <ul>
+                    <li> <a href="index.html">الرئيسية</a></li>
+                    <li> <a hef="regionsGallary.php" class="active">معرض المناطق</a></li>
+                    <li> <a href="AdminLogin.php">دخول المشرف</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+    </main>
+
+    <footer>  
+    </footer>
+
+</body>
+
+</html>
