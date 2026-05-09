7d489589679b70e3f834dbc4eefff0c63a6893cd
AlmahaDevv <444200477@Student.ksu.edu.sa>
2026-05-09 16:07:06 +0300

    dashboard + adminlog in edit

--- admin_pages/dashboard.php
+++ admin_pages/dashboard.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+// Check if the user is logged in, if not then redirect back to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: AdminLogin.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="ar">
 
@@ -13,7 +22,7 @@
     <main>
         <?php 
           //after adding a new region successfully the dashboard page should be opened with a massge
-          if (isset($_GET['success']) $$ $_['success']==1){
+          if (isset($_GET['success']) && $_GET['success']==1){
             echo '<div  id="alert-success" class="alert">تمت الاضافة بنجاح!</div>';
           }
         ?>
